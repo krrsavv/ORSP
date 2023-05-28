8cf67ba367f69ae6d023fd3dcb6851ac04c8befd
krrsavv <125188263+krrsavv@users.noreply.github.com>
2023-05-29 00:51:57 +0300

    Add files via upload

---
+++ savvinova_project/savvinova_app/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from .models import Users
+
+def index(request):
+    if request.POST.get('send') == 'Отправить':
+        Users.objects.create(
+            name=request.POST.get('user_name'),
+            phone=request.POST.get('user_phone'),
+            mail=request.POST.get('user_mail'),
+        )
+    return render(request, 'savvinova_app/index.html')
+
+
+def about(request):
+    return render(request, 'savvinova_app/about.html')
+
+
+def program(request):
+    return render(request, 'savvinova_app/program.html')
